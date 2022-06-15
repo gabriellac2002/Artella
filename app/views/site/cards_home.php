815854429c24b7c354d081878de43333346f14b1
Pedro Grimaldi <pedroghansel@gmail.com>
2022-06-15 14:43:12 -0300

    nova branch

---
+++ app/views/site/cards_home.php
@@ -0,0 +1,9 @@
+<div class="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-4 mx-0 cards">
+            <div class="card mx-auto">
+              <a href="#"><img src="https://2.bp.blogspot.com/-xqirkw3AdWE/UC1hr7Atu_I/AAAAAAAAD08/cQ2SMPUMdzM/s1600/abaporu.jpeg" class="card-img-top" alt="Abaporu"></a>
+              <div class="card-body">
+                <h5 class="card-title">Abaporu</h5>
+                <p class="card-text">R$100</p>
+              </div>
+            </div>
+          </div>
